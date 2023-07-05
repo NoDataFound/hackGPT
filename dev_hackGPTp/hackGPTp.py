f84d664d53715daec92a681354c924c46f87154c
Corian (Cory) Kennedy <cory@darkintel.io>
2023-07-05 11:56:32 -0500

    Update hackGPTp.py

--- dev_hackGPTp/hackGPTp.py
+++ dev_hackGPTp/hackGPTp.py
@@ -2,176 +2,72 @@ import streamlit as st
 from dotenv import load_dotenv
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
 from langchain.llms import GPT4All, LlamaCpp
+import argparse
+import time
 import os
-from PyPDF2 import PdfReader
-import tempfile
-import pytesseract
-from pdf2image import convert_from_path
+import subprocess
+import pandas as pd
+
+
 
 load_dotenv()
 
 # Set default values from .env
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 model_n_ctx = int(os.environ.get('MODEL_N_CTX', 1000))
 model_n_batch = int(os.environ.get('MODEL_N_BATCH', 8))
 target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS', 4))
 model_type = os.environ.get('MODEL_TYPE', 'GPT4All')
 model_path = os.environ.get('MODEL_PATH', 'LLM/ggml-gpt4all-j-v1.3-groovy.bin')
 embeddings_model_name = os.environ.get('EMBEDDINGS_MODEL_NAME', 'all-MiniLM-L6-v2')
-
+persist_directory = os.environ.get('PERSIST_DIRECTORY')
 # Set up the sidebar
-st.sidebar.title("Settings")
-with st.sidebar.expander("Settings"):
-    model_n_ctx = st.slider("MODEL_N_CTX", min_value=100, max_value=5000, value=model_n_ctx)
-    model_n_batch = st.slider("MODEL_N_BATCH", min_value=1, max_value=16, value=model_n_batch)
-    target_source_chunks = st.slider("TARGET_SOURCE_CHUNKS", min_value=1, max_value=10, value=target_source_chunks)
-    model_type = st.selectbox("MODEL_TYPE", ['LlamaCpp', 'GPT4All'], index=0 if model_type == 'LlamaCpp' else 1)
-    model_path = st.text_input("MODEL_PATH", value=model_path)
-    embeddings_model_name = st.text_input("EMBEDDINGS_MODEL_NAME", value=embeddings_model_name)
+
 
 from constants import CHROMA_SETTINGS
 
-def main():
-    # Load the embeddings model
-    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
-
-    # Set up the file upload
-    st.sidebar.title("File Upload")
-
-    # Create the "input/files" directory if it doesn't exist
-    os.makedirs("input/files", exist_ok=True)
-
-    # Read the uploaded files in the "input/files" directory
-    uploaded_files = [os.path.join("input/files", filename) for filename in os.listdir("input/files")]
-
-    # File uploader
-    uploaded_file = st.sidebar.file_uploader("Choose a file", type=["pdf", "txt"])
-    if uploaded_file is not None:
-        # Save the uploaded file
-        file_path = save_uploaded_file(uploaded_file)
-        st.sidebar.success("File uploaded successfully.")
-        uploaded_files.append(file_path)
-
-    # Multi-select for selecting documents
-    selected_documents = st.multiselect("Selected Documents", uploaded_files)
-
-    # Process button
-    if st.button("Process") and selected_documents:
-        with st.spinner("Processing the document..."):
-            try:
-                document_text = process_documents(selected_documents)
-                st.success("Document processed. Ready for questions.")
-                display_document_info(selected_documents, document_text)
-            except Exception as e:
-                st.error(f"Error: {str(e)}")
-
-    # Question form
-    query = st.text_input("Ask your question", value="", key="question_input")
-    submit_button = st.button("Submit")
-
-    document_text = ""  # Initialize the variable here
-    for document in selected_documents:
-        read_doc = document
-        with open(str(read_doc), "rb") as f:
-            if str(read_doc).lower().endswith('.pdf'):
-                pdf_reader = PdfReader(f)
-                num_pages = len(pdf_reader.pages)
-                images = convert_from_path(read_doc, dpi=300, first_page=1, last_page=num_pages)
-                with tempfile.TemporaryDirectory() as path:
-                    for i, image in enumerate(images):
-                        image_path = os.path.join(path, f"page_{i + 1}.jpg")
-                        image.save(image_path, "JPEG")
-                        text = pytesseract.image_to_string(image_path)
-                        document_text += text.strip() + " "
-            else:
-                document_text += f.read().decode("utf-8").strip() + " "
+os.makedirs("source_documents", exist_ok=True)
 
 
-    if submit_button and query:
-        st.title("Results")
+#st.set_page_config(page_title="𝚑𝚊𝚌𝚔🅶🅿🆃", page_icon="https://raw.githubusercontent.com/NoDataFound/hackGPT/main/res/hackgpt_fav.png", layout="wide")
+# Define the chat history data as a Pandas DataFrame
 
-        # Prepare the retrieval QA
-        db = Chroma(persist_directory="db", embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
-        retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
+CSS = """
+img {
+    box-shadow: 0px 10px 15px rgba(0, 0, 0, 0.2);
+}
+"""
 
-        # Prepare the LLM
-        if model_type == "LlamaCpp":
-            llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, n_batch=model_n_batch, verbose=False)
-        elif model_type == "GPT4All":
-            llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', n_batch=model_n_batch, verbose=False)
-        else:
-            raise Exception(f"Model type {model_type} is not supported. Please choose one of the following: LlamaCpp, GPT4All")
+st.markdown(f'<style>{CSS}</style>', unsafe_allow_html=True)
+st.sidebar.image('https://raw.githubusercontent.com/NoDataFound/hackGPT/main/res/hackGPT_logo.png', width=300)
+github_logo = "https://raw.githubusercontent.com/NoDataFound/hackGPT/main/res/github.png"
+hackGPT_repo = "https://github.com/NoDataFound/hackGPT"
 
-        qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
+st.sidebar.markdown(f"[![GitHub]({github_logo})]({hackGPT_repo} 'hackGPT repo')")
+st.sidebar.title("File Upload")
+st.image('https://raw.githubusercontent.com/NoDataFound/hackGPT/main/res/hackGPT_logo.png', width=800)    
+embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
+retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
 
-        # Get the answer from the chain
-        try:
-            st.info("Processing the question...")
-            chunks = split_text_into_chunks(document_text, model_n_ctx)
-            answer = ""
-            docs = []
-            for chunk in chunks:
-                res = qa(chunk, query)
-                answer += res['result'] + " "
-                docs.extend(res['source_documents'])
-            st.success("Question processed.")
-
-            # Print the result
-            st.subheader("Question:")
-            st.write(query)
-            st.subheader("Answer:")
-            st.write(answer.strip())
-
-            # Print the relevant sources used for the answer
-            for document in docs:
-                st.subheader(document.metadata["source"])
-                st.code(document.page_content)
-        except Exception as e:
-            st.error(f"Error: {str(e)}")
+uploaded_files = [os.path.join("source_documents", filename) for filename in os.listdir("source_documents") if filename != ".DS_Store"]
+uploaded_file = st.sidebar.file_uploader("Choose a file", type=["csv","docx","doc","enex","eml","epub","html","md","msg","odt","pdf","pptx ","ppt ","txt"])
 
 def save_uploaded_file(uploaded_file):
     file_name = uploaded_file.name
-    file_path = os.path.join("input/files", file_name)
+    file_path = os.path.join("source_documents", file_name)
     with open(file_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
+
+    # Call ingest.py script
+    subprocess.run(["python3", "ingest.py", file_path])
+
     return file_path
 
-def process_documents(selected_documents):
-    document_text = ""
-    for document_file in selected_documents:
-        with open(document_file, "r") as f:
-            text_content = f.read().strip()
-        document_text += text_content + " "
-    return document_text.strip()
 
-def display_document_info(selected_documents, document_text):
-    # Document information
-    st.subheader("Document Information")
-
-    # Display file names
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.write("Filename:")
-        for document_file in selected_documents:
-            st.write(os.path.basename(document_file))
-
-    # Display word count
-    with col2:
-        word_count = len(document_text.split())
-        st.write("Word Count:")
-        st.write(word_count)
-
-    # Display total length
-    with col3:
-        total_length = len(document_text)
-        st.write("Total Length:")
-        st.write(total_length)
-
-    # Display five lines of text
-    #st.write("Text Sample:")
-    lines = document_text.split("\n")[:5]
-    st.code("\n".join(lines))
 
 def split_text_into_chunks(text, chunk_size):
     chunks = []
@@ -181,6 +77,110 @@ def split_text_into_chunks(text, chunk_size):
     if text:
         chunks.append(text)
     return chunks
+ 
+if uploaded_file is not None:
+    file_path = save_uploaded_file(uploaded_file)
+    st.sidebar.success("File uploaded successfully.")
+    uploaded_files.append(file_path)
+uploaded_files = [os.path.join("source_documents", filename) for filename in os.listdir("source_documents") if filename != ".DS_Store"]
+
+df_data = []
+
+total_words = 0  # Variable to store the total word count
+
+for idx, document_file in enumerate(uploaded_files):
+    file_name = os.path.basename(document_file)
+    file_type = os.path.splitext(file_name)[1].lstrip('.')
+    date_trained = os.path.getmtime(document_file)
+    word_count = 0
+    sample = ""
+
+    if file_type.lower() != "pdf":  # Skip line reading for PDF files
+        with open(document_file, "r") as f:
+            lines = f.readlines()
+            if len(lines) > 0:
+                word_count = sum(len(line.split()) for line in lines)  # Count words in each line
+                sample = lines[0].strip()
+        
+            total_words += word_count  # Add current document's word count to the total
+
+    df_data.append({
+        'File Name': file_name,
+        'File Type': file_type,
+        'Date Trained': pd.to_datetime(date_trained, unit='s').strftime('%m-%d-%y'),
+        'Word Count': word_count,
+        'Sample': sample
+    })
+
+df = pd.DataFrame(df_data)
+
+# Sidebar options
+st.sidebar.title("Training Data")
+show_training_data = st.sidebar.checkbox("Show Training Data")
+selected_files = st.sidebar.multiselect("Select Files to Re-process", uploaded_files)
+delete_training_data = st.sidebar.button("Delete Selected Files")
+reprocess_training_data = st.sidebar.button("Re-process Selected Files")
+
+if delete_training_data:
+    # Delete selected files logic here
+    for file_path in selected_files:
+        os.remove(file_path)
+    st.sidebar.success("Selected files deleted.")
+    st.stop()
+
+if reprocess_training_data:
+    # Reprocess selected files logic here
+    for file_path in selected_files:
+        subprocess.run(["python3", "ingest.py", file_path])
+    st.sidebar.success("Selected files re-processed.")
+    st.stop()
+
+if show_training_data:
+    st.info("Training Data")
+    st.dataframe(df.style.set_properties(subset=['Date Trained'], **{'font-size': '12px'}))
+
+def main():
+    # Load the embeddings model
+    args = parse_arguments()
+    callbacks = [] if args.mute_stream else [StreamingStdOutCallbackHandler()]
+    
+    if model_type == "LlamaCpp":
+        llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+    elif model_type == "GPT4All":
+        llm = GPT4All(model=model_path, n_ctx=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+    else:
+        raise Exception(f"Model type {model_type} is not supported. Please choose one of the following: LlamaCpp, GPT4All")
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=not args.hide_source)
+    
+    query = st.text_input("", value="Ask your question", key="question_input")
+    submit_button = st.button("Submit")
+
+    if submit_button:
+        st.spinner("Processing Question")
+        start = time.time()
+        res = qa(query)
+        answer, docs = res['result'], [] if args.hide_source else res['source_documents']
+        end = time.time()
+        
+        st.code(f"> Answer (took {round(end - start, 2)} s.):")
+        st.success(answer)
+
+        for document in docs:
+            st.code("'Answer derived from "+ document.metadata["source"]+ " in this section: ") 
+            st.info(document.page_content)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='privateGPT: Ask questions to your documents without an internet connection, '
+                                                 'using the power of LLMs.')
+    parser.add_argument("--hide-source", "-S", action='store_true',
+                        help='Use this flag to disable printing of source documents used for answers.')
+
+    parser.add_argument("--mute-stream", "-M",
+                        action='store_true',
+                        help='Use this flag to disable the streaming StdOut callback for LLMs.')
+
+    return parser.parse_args()
 
 if __name__ == "__main__":
     main()
