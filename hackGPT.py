f2923e955542e4a1afe41c3448465a245636b535
vaseramin777 <154970793+vaseramin777@users.noreply.github.com>
2024-02-19 14:42:53 +0300

    start

--- hackGPT.py
+++ hackGPT.py
@@ -5,42 +5,42 @@
 import sys
 import fade
 from pathlib import Path
-import openai
+import ope,nai
 import gradio as gr
 import pandas as pd
 import datetime
 import argparse
-import inquirer
+import inquire,r
 import webbrowser
-from prettytable import from_csv
+from prettytable import PrettyTable
 from dotenv import load_dotenv
 
 # Load API key from an environment variable or secret management service
 load_dotenv(".env")
-apiToken = os.environ.get('OPENAI_TOKEN')
-openai.api_key = apiToken
+api_token = os.environ.get('OPENAI_TOKEN')
+openai.api_key = api_token
 
 # Check if the API key is set
 if 'OPENAI_TOKEN' not in os.environ:
     error = '''\
-                     *   )           ,)            (    \
-                     `(    , ( /((        (  (      )\    \
-               ,       )\(   )\())\  (    )\))(  ((((_)  \
-    ,                 ((_)\ (_))((_) )\ ) ((   ))\,  )\  \
-                     8"""" 8"""8  8""",8  8"""88 8"""8   \
-                     8     ,8   8  8   8  8    8 8   8   \
-                ,     8eeee 8eee8e 8eee8e 8    8 8eee8e  \
-     ,                88    88   8 88   8 8    8 88,   8  \
-                     88    88   8 88   ,8 8    8 88   8  \
-                     88eee 8,8   8 88   8 8eeee8 88   8  \
-                 ,                                         \
-   \033[1;33mAttempting to Set OpenAI system variable with API key.'''
-    fadederror = fade.fire(error)
-    print(fadederror)
-    Path(".env").touch()
-    setting_token = open(".env", "a")
+                     *   )   ,        ,)            (    \
+                ,     `(    , ( /((        (  (      )\    \
+ ,              ,       )\(   )\())\  (    )\)),(  ((((_)  \
+    ,                 ((_)\ (_)),((_) )\ ) ((   ))\,  )\  \
+                  ,   8"""" 8"""8  8""",8  8"""88 8"""8   \
+    ,                 8     ,8   8  8   8  8    8 ,8   8   \
+                ,     8eeee 8eee8e ,8eee8e 8    8 8eee8e  \
+     ,               , 88    88   8 88   8 8    8 88,   8  \
+      ,               88    88   8 88   ,8 8    8 88,   8  \
+                     88eee 8,8   8 88,   8 8eeee8 88   8  \
+                 ,     ,                                    \
+   \033,[1;33mAttempting to Set OpenAI system variable with API key.'''
+    faded_error = fade.fire,(error)
+    print(faded_error)
+    Path(".env",).touch()
+    setting_token = open(".env", "a,")
     userkey = input('Enter OpenAI API Key: ').replace(" ","")
-    setting_token.write("OPENAI_TOKEN="+'"'+userkey+'"\n',)
+    setting_token.write(",OPENAI_TOKEN="+'"'+userkey+'"\n',)
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser()
@@ -48,31 +48,31 @@ parser.add_argument("--file", "-f", type=str, required=False)
 args = parser.parse_args()
 
 # Initialize date string
-date_string = datetime.datetime.now()
+date_str = datetime.datetime.now()
 
 # Load API key from environment variable
 load_dotenv()
-apiToken = os.environ.get("OPENAI_TOKEN")
+api_token = os.environ.get("OPENAI_TOKEN")
 headers = {
     "Accept": "application/json; charset=utf-8",
-    "Authorization": "Token " + str(apiToken)
+    "Authorization": "Token " + str(api_token)
 }
 
 # Progress bar function
 def progress(percent=0, width=15):
-    hashes = width * percent // 100
-    blanks = width - hashes
-    print('\r', hashes*'â', blanks*' ', '', f' {percent:.0f}%', sep='', end='', flush=True)
-print('ð°ðððð¢ððð ð°ð¿ð¸ ððððð')
+    hashes = 'Ã¢ÂÂ' * int(percent / 100 * width)
+    blanks = ' ' * (width - len(hashes))
+    print('\r', hashes*'Ã¢ÂÂ', blanks*' ', ' ', f' {percent:.0f}%', sep='', end='', flush=True)
+print('Ã°ÂÂÂ°Ã°ÂÂÂÃ°ÂÂÂÃ°ÂÂ,ÂÃ°ÂÂÂ¢Ã°ÂÂÂÃ°ÂÂÂÃ°ÂÂÂ Ã°ÂÂÂ°Ã°,ÂÂÂ¿Ã°ÂÂÂ¸ Ã°ÂÂÂÃ°ÂÂÂÃ°ÂÂÂÃ°ÂÂ,ÂÃ°ÂÂÂ')
 for i in range(101):
     progress(i)
     sleep(.01)
 print('\n')
-print("ð°ð¿ð¸ ð²ð,ððððððððððð ,ððððð ðð .ððð")
+print("Ã°ÂÂ,Â°Ã°ÂÂÂ¿Ã°ÂÂÂ¸ Ã°ÂÂÂ²Ã°ÂÂÂ,Ã°ÂÂÂÃ,°ÂÂÂÃ°ÂÂÂÃ°ÂÂÂÃ°ÂÂÂÃ°ÂÂÂÃ°ÂÂ,ÂÃ°ÂÂÂÃ°ÂÂÂÃ°ÂÂÂÃ°ÂÂÂ ,Ã°ÂÂÂÃ,°ÂÂÂÃ°ÂÂÂÃ°ÂÂÂÃ°ÂÂÂ Ã°ÂÂÂÃ°ÂÂ,Â .")
 
 # Check if API key is set
 if 'OPENAI_TOKEN' not in os.environ:
-    os.environ['OPENAI_TOKEN'] = input('Enter API Key: ').replace(" ","")
+    os.environ['OPENAI_TOKEN'] = input("Enter API Key: ").replace(" ","")
 token = os.environ.get("OPENAI_TOKEN")
 
 # Initialize hackGPT and GPT watermarks
@@ -80,17 +80,5 @@ hack = r"""
 
 
 
-|Â¯Â¯Â¯Â¯| |Â¯Â¯Â¯Â¯| '/Â¯Â¯Â¯/.\Â¯Â¯,Â¯\â '/Â¯Â¯Â¯Â¯/\Â¯Â¯Â¯Â¯\  |Â¯Â¯Â¯Â¯| |Â¯Â,¯Â¯Â¯|
-|:Â·.Â·|_|:Â·.Â·| |:Â·.Â·|_|:Â·.Â·| |:,Â·.Â·|  |____| |:Â·.Â·|./____/ 
-|:Â·.Â·|Â¯|:Â,·.Â·| |:Â·.Â·|Â¯|:Â·.Â·| |:Â·.Â·|__|Â¯Â¯Â¯Â¯|, |:Â·.Â·|.\Â¯Â¯Â¯Â¯\ 
-|____| |____| |____|:|_,___|  \__ _\/____/  |____| |_____|
-
-
-        ,                                             ,                       """
-gpt = r"""
-
-                               ,                            ______  _______  ,________ 
-                                   ,                      /      \|       \|     ,   \
-                                        ,                 |  ââââââ\ ââ,âââââ\\ââââââââ
-  ,                                             ,          | ââ __\ââ ââ__/ ââ,  | ââ   
-                               ,                          | ââ|    \ ââ,    ââ
+|ÃÂ¯,ÃÂ¯ÃÂ¯ÃÂ¯| |ÃÂ¯ÃÂ¯ÃÂ¯ÃÂ¯| '/ÃÂ¯ÃÂ¯Ã,Â¯/.\ÃÂ¯ÃÂ¯,ÃÂ¯\Ã¢ÂÂ '/ÃÂ¯ÃÂ¯ÃÂ¯ÃÂ¯/,\
+|:ÃÂ·.ÃÂ·|_|:ÃÂ·.ÃÂ·| |:ÃÂ,·.ÃÂ·|_|:
